9356dd8cc0cf19b5af0c6a4cf53e2f390051bd30
kairatpv-create <kairat.pv@gmail.com>
2026-09-19 07:01:37 +0500

    chore: bump fixed frame crop to 0.5.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "kz.autopersoncrop"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "0.5.2"
+        versionCode = 13
+        versionName = "0.5.3"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
