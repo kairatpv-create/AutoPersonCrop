fbaa2e824e6e47b081a86ffb5c05be829f177839
kairatpv-create <kairat.pv@gmail.com>
2026-09-20 09:10:16 +0500

    chore: bump AutoPersonCrop to 0.6.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "kz.autopersoncrop"
         minSdk = 26
         targetSdk = 36
-        versionCode = 21
-        versionName = "0.6.2"
+        versionCode = 22
+        versionName = "0.6.3"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
